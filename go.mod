c1fa19c7b243fefc418822133bc9082d024577d9
ibarnold19 <ibarnold19@gmail.com>
2024-11-03 23:33:27 -0500

    more testing

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ibarnold19/docker-learning-practice
+
+go 1.23.2
--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 )
 
@@ -12,7 +13,7 @@ func main() {
 
 	m.HandleFunc("/", handlePage)
 
-	const addr = ":8080"
+	addr := fmt.Sprintf(":%s", os.Getenv("PORT"))
 	srv := http.Server{
 		Handler:      m,
 		Addr:         addr,
@@ -33,7 +34,7 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	const page = `<html>
 <head></head>
 <body>
-	<p> Hello from Docker! I'm a Go server. </p>
+	<p> Hi Docker, I pushed a new version! </p>
 </body>
 </html>
 `
